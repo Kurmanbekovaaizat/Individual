87bf88ba077a0a13b83b1028c6ed47ba75a0d004
kurmanbekovaaizat <aizada_kurmanbekova@mail.ru>
2016-12-26 12:42:57 +0600

    Indi

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv.h>
+#include <math.h>
+using namespace std;
+using namespace cv;
+void Function();
+int main() {
+    Function();
+    return 0;
+}
+void Function(){
+    Mat img = cvLoadImage("/home/aizat/IG/pervoe.jpg");
+    cvtColor(img, img, CV_RGB2GRAY);
+    Mat res = img.clone();
+    int mask[3][3] = { {3, 0, -1},
+                       {2, 2, 0},
+                       {1, 1, 1}
+    };
+    for (int i = 0; i < img.rows-1; i++){
+        for (int j = 0; j < img.cols-1; j++){
+            int sum = 0;
+            for (int k = -1; k < 2; k++){
+                for (int l = -1; l < 2; l++){
+                    sum += (img.at<uchar>((i+k),(j+l))*mask[k+1][l+1]);
+                }
+            }
+            if (sum < 0){
+                sum*=-1;
+            }
+            if (sum > 255){
+                sum = 255;
+            }
+            res.at<uchar>(i,j) = sum;
+        }
+    }
+    cvNamedWindow("Pixel",1);
+    imshow("Pixel", res);
+    waitKey(0);
+
+
+}
